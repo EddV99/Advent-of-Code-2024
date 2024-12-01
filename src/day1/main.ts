2a3077e25d43478675375333b773ca16d17f4e90
Eduardo Valdivia <eddval0808@gmail.com>
2024-12-01 16:09:21 -0700

    day 1 part 1 ⭐

--- src/day1/main.ts
+++ src/day1/main.ts
@@ -1,7 +1,47 @@
-function part1() {}
+import getFile from "./../util/read-file.js";
 
-function part2() {}
+// input file contents
+let inputText = getFile("./../../inputs/day1/input.txt");
+
+// example contents
+const example1: string = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+let leftList: string[] = [];
+let rightList: string[] = [];
+
+// parse input
+let input = inputText.split("\n");
+input.forEach((line) => {
+  let tokens = line.split(/\s+/);
+  if (tokens.length == 2 && tokens[0].length > 0 && tokens[1].length > 0) {
+    leftList.push(tokens[0]);
+    rightList.push(tokens[1]);
+  }
+});
+
+function part1(): number {
+  let sortedLeftList = [...leftList].sort();
+  let sortedRightList = [...rightList].sort();
+
+  let sum = 0;
+  for (let i = 0; i < sortedLeftList.length; ++i) {
+    sum += Math.abs(Number(sortedLeftList[i]) - Number(sortedRightList[i]));
+  }
+
+  return sum;
+}
+
+function part2(): number {
+  return 0;
+}
 
 export default function run() {
-  console.log("Hello from day1");
+  console.log("Day 1 Solution");
+  console.log("Part 1:\n", part1());
+  console.log("Part 2:\n", part2());
 }
