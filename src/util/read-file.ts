8768129e1c0c7b8b10575b7d80d100fce39c0cf3
Eduardo Valdivia <eddval0808@gmail.com>
2024-12-01 16:08:00 -0700

    use correct function to read file

--- src/util/read-file.ts
+++ src/util/read-file.ts
@@ -1,19 +1,7 @@
-import { readFile } from "fs";
+import { readFileSync } from "fs";
 import * as path from "path";
 
 export default function getFile(filepath: string): string {
-  let fileContents: string = "";
   filepath = path.resolve(import.meta.dirname, filepath);
-
-  console.log(filepath);
-
-  readFile(filepath, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error getting file: ", err);
-      return;
-    }
-    fileContents = data;
-  });
-
-  return fileContents;
+  return readFileSync(filepath, "utf8");
 }
