d2358101c26e4003ccf32611e1bf4b2a5129c99f
Eduardo Valdivia <eddval0808@gmail.com>
2024-12-01 16:25:08 -0700

    day 1 part 2 ⭐

--- src/day1/main.ts
+++ src/day1/main.ts
@@ -1,7 +1,6 @@
 import getFile from "./../util/read-file.js";
-
 // input file contents
-let inputText = getFile("./../../inputs/day1/input.txt");
+let inputFile = getFile("./../../inputs/day1/input.txt");
 
 // example contents
 const example1: string = `3   4
@@ -14,8 +13,10 @@ const example1: string = `3   4
 let leftList: string[] = [];
 let rightList: string[] = [];
 
+const INPUT_TEXT = inputFile;
+
 // parse input
-let input = inputText.split("\n");
+let input = INPUT_TEXT.split("\n");
 input.forEach((line) => {
   let tokens = line.split(/\s+/);
   if (tokens.length == 2 && tokens[0].length > 0 && tokens[1].length > 0) {
@@ -37,7 +38,32 @@ function part1(): number {
 }
 
 function part2(): number {
-  return 0;
+  let leftMap = new Map<string, number>();
+  let rightMap = new Map<string, number>();
+
+  for (let i = 0; i < leftList.length; i++) {
+    let lv = leftList[i];
+    let rv = rightList[i];
+
+    if (leftMap.has(lv)) {
+      leftMap.set(lv, leftMap.get(lv)! + 1);
+    } else {
+      leftMap.set(lv, 1);
+    }
+
+    if (rightMap.has(rv)) {
+      rightMap.set(rv, rightMap.get(rv)! + 1);
+    } else {
+      rightMap.set(rv, 1);
+    }
+  }
+
+  let sum = 0;
+  leftMap.forEach((value, key) => {
+    if (rightMap.has(key)) sum += value * Number(key) * rightMap.get(key)!;
+  });
+
+  return sum;
 }
 
 export default function run() {
