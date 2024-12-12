e50575d18f7dbc759da5d47a853c70ae94c06076
Eduardo Valdivia <eddval0808@gmail.com>
2024-12-12 13:28:26 -0700

    day 6 part 2 ⭐

--- src/day6/main.ts
+++ src/day6/main.ts
@@ -14,6 +14,16 @@ const example1: string = `....#.....
 #.........
 ......#...`;
 
+// edge case where we need to rotate twice
+// .#..
+// ..#.
+// .^..
+// ....
+const example2: string = `.#..
+..#.
+.^..
+....`;
+
 const INPUT_TEXT = inputFile;
 
 let grid: string[][] = [];
@@ -41,6 +51,9 @@ lines.forEach((line, r) => {
     dir = { r: 0, c: -1 };
   }
 });
+
+let start_pos = { r: pos.r, c: pos.c };
+let start_dir = { r: dir.r, c: dir.c };
 
 let rows = grid.length;
 let cols = grid[0].length;
@@ -75,13 +88,17 @@ function part1() {
   while (pos.r < rows && pos.r >= 0 && pos.c < cols && pos.c >= 0) {
     if (!(pos.r + dir.r < rows && pos.r + dir.r >= 0 && pos.c + dir.c < cols && pos.c + dir.c >= 0)) {
       count++;
+      grid[pos.r][pos.c] = "X";
       break;
     }
-    let oncoming = grid[pos.r + dir.r][pos.c + dir.c];
-    // oncoming is an obsticle
-    if (oncoming !== "." && oncoming !== "X") {
+
+    while (grid[pos.r + dir.r][pos.c + dir.c] !== "." && grid[pos.r + dir.r][pos.c + dir.c] !== "X") {
       rotate(dir);
+      if (!(pos.r + dir.r < rows && pos.r + dir.r >= 0 && pos.c + dir.c < cols && pos.c + dir.c >= 0)) {
+        return count;
+      }
     }
+
     if (grid[pos.r][pos.c] !== "X") count++;
     grid[pos.r][pos.c] = "X";
     pos.r += dir.r;
@@ -91,7 +108,58 @@ function part1() {
   return count;
 }
 
-function part2() {}
+let checkCycle = () => {
+  pos.r = start_pos.r;
+  pos.c = start_pos.c;
+
+  dir.r = start_dir.r;
+  dir.c = start_dir.c;
+
+  let visited = new Set<string>();
+
+  while (pos.r < rows && pos.r >= 0 && pos.c < cols && pos.c >= 0) {
+    // we went outside, so no cycle
+    if (!(pos.r + dir.r < rows && pos.r + dir.r >= 0 && pos.c + dir.c < cols && pos.c + dir.c >= 0)) {
+      return false;
+    }
+
+    let info = JSON.stringify({ pos: { x: pos.c, y: pos.r }, dir: { x: dir.c, y: dir.r } });
+    // if already visited in same direction then it's a cycle
+    if (visited.has(info)) return true;
+    else visited.add(info);
+
+    while (grid[pos.r + dir.r][pos.c + dir.c] === "#") {
+      rotate(dir);
+      if (!(pos.r + dir.r < rows && pos.r + dir.r >= 0 && pos.c + dir.c < cols && pos.c + dir.c >= 0)) {
+        return false;
+      }
+    }
+
+    pos.r += dir.r;
+    pos.c += dir.c;
+  }
+
+  return false;
+};
+
+function part2() {
+  // NOTE: only works because part1 has filled the grid with Xs
+  // Xs represent path the guard takes
+  let count = 0;
+  for (let r = 0; r < rows; ++r) {
+    for (let c = 0; c < cols; ++c) {
+      // only check for places the guard might go
+      if (grid[r][c] === "X" && !(r === start_pos.r && c === start_pos.c)) {
+        // place the obstruction and check if cycle
+        grid[r][c] = "#";
+        if (checkCycle()) count++;
+        grid[r][c] = "X";
+      }
+    }
+  }
+
+  return count;
+}
 
 export default function run() {
   console.log("Day 6 Solution");
